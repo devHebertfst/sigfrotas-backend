0b143130672e8bff74d2759367cde1b05319cf80
Hebert França <hebertfst@hotmail.com>
2024-11-27 21:49:58 -0300

    init commit

---
+++ src/test/java/br/ufrn/imd/sigfrotas_backend/SigfrotasBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ufrn.imd.sigfrotas_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SigfrotasBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
